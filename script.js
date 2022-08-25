bb45a121acd4f8a8be1f316e8455ceeef7246d93
AldanaNaser6 <aldananasser.2007@gmail.com>
2022-08-25 20:48:31 +0300

    cw9

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let ordars = [];
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,18 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = { name, id, phone, order };
   // أدرج الكائن إلى المصفوفة
-  
+  ordars.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
-}
+  load();
 
 function load() {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
-
+ordars .forEach(item => {
+container.innerHTML += `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+  });
+}
 }
